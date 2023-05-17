2fa96c67ed25427a54cde96d751b4c1b84dd6afc
JunTaeINC <juntea3523@naver.com>
2023-05-17 14:28:31 +0900

    feat : SecurityConfig / 간단한 테스트

---
+++ src/main/java/com/ll/rest/boundedContext/member/controller/memberController.java
@@ -0,0 +1,16 @@
+package com.ll.rest.boundedContext.member.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class memberController {
+
+    @PostMapping("/login")
+    public String login() {
+        return "Hi";
+
+    }
+}
